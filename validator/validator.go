bca0bae7c5d292a2b6695acc4bfe3ac1656599ea
ikaiguang <ikaiguang@163.com>
2019-04-08 08:56:55 +0000

    is_id_card

--- validator/validator.go
+++ validator/validator.go
@@ -36,3 +36,14 @@ func IsValidPassword(password string) bool {
 
 	return regexp.MustCompile(passwordReg).MatchString(password)
 }
+
+// IsIDCard : id card
+func IsIDCard(idCard string) bool {
+	var idCardReg string
+	if len(idCard) == 15 {
+		idCardReg = `^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$`
+	} else {
+		idCardReg = `^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$`
+	}
+	return regexp.MustCompile(idCardReg).MatchString(idCard)
+}
